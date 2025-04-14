46f6d04f53059b9e99279d116c9948ca77831d78
rmhockey10 <rees.m.hannah@gmail.com>
2025-04-14 14:26:20 -0400

    done

---
+++ app.js
@@ -0,0 +1,42 @@
+//Import the express library
+import express from "express";
+
+//Create an express app
+const app = express();
+
+//Export the app
+export default app;
+
+import employees from "#db/employees";
+
+console.log(employees);
+
+// GET / to send the message "Hello World"
+app.route("/").get((request, response) => {
+  response.send("Hello Employees!");
+});
+
+// GET /employees/random randomly generated a number used as an id to select an employee
+app.route("/employees/random").get((request, response) => {
+  const id = Math.floor(Math.random() * employees.length);
+  console.log(id);
+  const found = employees.find((employee) => employee.id === id);
+  response.send(found.name);
+});
+
+//GET /employees sends array of employees
+app.route("/employees").get((request, response) => {
+  response.send(employees);
+});
+
+// GET /employees/:id sends the employee with the given id number
+app.route("/employees/:id").get((request, response) => {
+  const { id } = request.params;
+  console.log(id);
+
+  if (!employees || +id > employees.length || +id < 1) {
+    return response.status(404).send("there is no employee with that id");
+  }
+  const found = employees.find((employee) => employee.id === +id);
+  response.send(found.name);
+});
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+//import app from app.js file
 import app from "#app";
+//define a port constant
 const PORT = 3000;
+//tell the app to listen on this port and pring something
+//to let us know it's up and running
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
